ae305d90eda2fda8d310246ccb752aa37186c618
HackDehZack <Zackseriousemail@gmail.com>
2024-02-14 14:55:47 -0500

    Add Thought and Reaction models

---
+++ models/index.js
@@ -0,0 +1,8 @@
+// Purpose: This file will import the User and Thought models and export them as an object so we can easily access them in other parts of thou application.
+const Thought = require('./Thought');
+const User = require('./User');
+
+
+
+// Export the models
+module.exports = { Thought, User };
