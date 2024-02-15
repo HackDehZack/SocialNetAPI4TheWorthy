a1b69e974a83c42c908dcd25c24327bee41991d9
HackDehZack <Zackseriousemail@gmail.com>
2024-02-14 20:14:33 -0500

    Fix import statement in user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,7 +7,7 @@ const {
     deleteUser,
     addFriend,
     removeFriend
-} = require("../../controllers/userController");
+} = require("../../controllers/usercontroller");
 
 router.route("/")
     .get(getAllUser)
